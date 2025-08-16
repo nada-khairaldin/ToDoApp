16b54062d79b32c6f20beb9daaadc0dc60117af2
nada khairaldin <nadakhairaldin8@gmail.com>
2025-08-16 14:54:28 +0300

    add new tasks on click or Enter, ignore empty input

---
+++ script.js
@@ -0,0 +1,40 @@
+const toDoList = document.getElementById("to-do-list");
+const addBtn = document.getElementById("add-btn");
+const addInput = document.getElementById("add-input");
+
+function addHandler(e) {
+  e.preventDefault();
+  const addInputValue = addInput.value.trim();
+  if (!addInputValue) return;
+  createTask(addInputValue);
+  
+
+}
+
+function createTask(addInputValue){
+const taskContainer = document.createElement("div");
+  taskContainer.classList.add("task");
+
+  const leftSide = document.createElement("div");
+  const checkBox = document.createElement("input");
+  checkBox.setAttribute("type", "checkbox");
+  const newTask = document.createElement("label");
+  newTask.textContent = addInputValue;
+  leftSide.append(checkBox, newTask);
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("delete-btn")
+
+  taskContainer.append(leftSide, deleteBtn);
+  toDoList.append(taskContainer);
+  addInput.value ="";
+}
+
+
+
+addBtn.addEventListener("click", addHandler);
+addInput.addEventListener("keypress" , (e) => {
+  if(e.key === "Enter"){
+    addHandler(e)
+  }
+})
